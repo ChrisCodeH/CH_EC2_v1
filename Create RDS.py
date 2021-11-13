1068d20d3e09c0d083049a3eddd49b08e5a3717d
Christopher Howard <chrishowarduk@live.co.uk>
2021-11-13 19:15:36 +1100

    Update Create RDS.py

--- Create RDS.py
+++ Create RDS.py
@@ -4,7 +4,8 @@
 import boto3
 import time
 rds = boto3.client('rds')
-dbid = 'chdb-instance-1'
+dbid = 'chdb-instance-2'
+checkInterval = 10
 
 response = rds.create_db_instance(
     AllocatedStorage=5,
@@ -23,18 +24,34 @@ print('DB Status: ' + response['DBInstance']['DBInstanceStatus']);
 #client.describe_db_instances(DBInstanceIdentifier='chdb-instance-1') 
 
 while(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus']!='available'):  #Check for Available State
-        time.sleep(10)
+        time.sleep(checkInterval)
         print(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus'])
         
 print(dbid + ' is now Available');
 print(' ')
-print('Now Stopping and Deleting' + dbid);
+print('Now Stopping ' + dbid);
 print(' ')
 result = rds.stop_db_instance(
     DBInstanceIdentifier=dbid
 )
-print(result)
+print('DB Status: ' + result['DBInstance']['DBInstanceStatus'])
 
-while(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus']!='deleted'):  #Check for Available State
-        time.sleep(10)
+while(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus']!='stopped'):  #Check for stopped State
+        time.sleep(checkInterval)
         print(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus'])
+
+print(dbid + ' is now Stopped');
+print(' ')
+print('Now Deleting ' + dbid);
+print(' ')
+        
+finalresponse = client.delete_db_instance(
+    DBInstanceIdentifier=dbid,
+    SkipFinalSnapshot=True,
+    DeleteAutomatedBackups=True
+)
+
+print('DB Status: ' + finalresponse['DBInstance']['DBInstanceStatus'])
+
+if(finalresponse['DBInstance']['DBInstanceStatus']=='deleting'):
+    print(dbid + ' is being deleted. We are Done!')
