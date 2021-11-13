4f391fc30ddf9fa575c1f6329af9da4afa8f1406
Christopher Howard <chrishowarduk@live.co.uk>
2021-11-13 17:57:05 +1100

    Update Create EC2.py

--- Create EC2.py
+++ Create EC2.py
@@ -1,9 +1,10 @@
 import boto3
+import time
 ec2 = boto3.resource('ec2')
-sts = boto3.client('sts')
-account_id = sts.get_caller_identity()['Account']
+#sts = boto3.client('sts')
+#account_id = sts.get_caller_identity()['Account']
 
-#create a new EC2 instance
+#create the ec2 instances
 instances = ec2.create_instances(
      ImageId='ami-043e0add5c8665836',
      MinCount=1,
@@ -12,19 +13,40 @@ instances = ec2.create_instances(
      KeyName='ec2-keypair',
  )
  
-
+for instance in instances:
+    print('Instance Created (ID): ' + instance.id)    
+    
 count=1
 for instance in instances:
-    instance_arn=f"arn:aws:ec2:{boto3.Session().region_name}:{account_id}:instance/{instance.id}"
-    print(instance_arn)
-    print(instance.public_ip_address)
-    print(instance.state)
+    #Debug Commands
+    #instance_arn=f"arn:aws:ec2:{boto3.Session().region_name}:{account_id}:instance/{instance.id}"
+    #print(instance_arn)
+    #print(instance.public_ip_address)
+    #print(instance.state)
+    
+    #Tag instances
     instance.create_tags(
     DryRun=False,
     Tags=[
         {
             'Key': 'project',
-            'Value': str(++count)
+            'Value': str(count)
         },
     ]
-)
+    )
+    count += 1
+    
+    #Check IPs
+    print("Waiting for IP Assignment..")
+    while(instance.public_ip_address==None):  #Keep Checking for PIP assignment
+        time.sleep(5)
+        instance.reload() 
+        print("Waiting for IP Assignment..")
+    
+    print(' ') 
+    print('################################') 
+    print('Instance ID: ' + instance.id)    
+    print('Public IP: ' + instance.public_ip_address)
+    print('State: ' + instance.state.get('Name'))
+    print('################################') 
+    print(' ') 
