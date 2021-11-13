bab5cb97680aad0dd12a6056a8266cc9454163a1
Christopher Howard <chrishowarduk@live.co.uk>
2021-11-13 19:04:29 +1100

    Update Create RDS.py

--- Create RDS.py
+++ Create RDS.py
@@ -1,6 +1,40 @@
-#Create 2 RDS Instance, Start, Stop and Terminate
+#Create RDS Instance, Start, Stop and Terminate
 #Created By Christopher Howard
 
 import boto3
 import time
-rds = boto3.resource('rds')
+rds = boto3.client('rds')
+dbid = 'chdb-instance-1'
+
+response = rds.create_db_instance(
+    AllocatedStorage=5,
+    DBInstanceClass='db.t2.micro',
+    DBInstanceIdentifier=dbid ,
+    Engine='MySQL',
+    MasterUserPassword='dbadmin!',
+    MasterUsername='dbadmin',
+)
+
+print(response)
+print('DB Status: ' + response['DBInstance']['DBInstanceStatus']);
+
+
+
+#client.describe_db_instances(DBInstanceIdentifier='chdb-instance-1') 
+
+while(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus']!='available'):  #Check for Available State
+        time.sleep(10)
+        print(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus'])
+        
+print(dbid + ' is now Available');
+print(' ')
+print('Now Stopping and Deleting' + dbid);
+print(' ')
+result = rds.stop_db_instance(
+    DBInstanceIdentifier=dbid
+)
+print(result)
+
+while(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus']!='deleted'):  #Check for Available State
+        time.sleep(10)
+        print(rds.describe_db_instances(DBInstanceIdentifier=dbid )['DBInstances'][0]['DBInstanceStatus'])
